f52bd54c54c75858df30748f1f56408d6dfe40d2
Ashes Chattopadhyay <ashes@ashess-mbp.attlocal.net>
2018-07-12 20:55:53 -0500

    added comments

--- cnnv13_4convlayers.py
+++ cnnv13_4convlayers.py
@@ -6,39 +6,46 @@ import numpy as np
 
 from numpy import genfromtxt
 
-
+#define hyper-parameters,no. of epochs, batch size
 learning_rate = 0.0001
 num_steps = 500
 batch_size = 32
 display_step = 10
 
-#loss_fn=np.zeros((int(np.size(label,0)/batch_size)*(num_steps+1)));
-num_input =28*28  # MNIST data input (img shape: 28*28)
-num_classes = 4 # MNIST total classes (0-9 digits)
-dropout = 0.5 # Dropout, probability to keep units
-X = tf.placeholder(tf.float32, [None, num_input*3])
+
+num_input =28*28  # Z500 data input (img shape: 28*28)
+num_classes = 4 # weather patterns total classes is 4
+dropout = 0.5 # Dropout, probability to keep units. Don't change this 
+# These are placeholders in computation graph
+X = tf.placeholder(tf.float32, [None, num_input*3]) 
 Y = tf.placeholder(tf.float32, [None, num_classes])
+# placeholder for dropout in feed forward layer
 keep_prob = tf.placeholder(tf.float32) # dropout (keep probability)
+
+#define convolution layer in 2D. Requires data input weights, and stride always equ#al to 1 
 
 def conv2d(x, W, b, strides=1):
     # Conv2D wrapper, with bias and relu activation
     x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')
     x = tf.nn.bias_add(x, b)
     return tf.nn.relu(x)
+
+#max pool function. k=2 always unless you wanna change more stuff
 def maxpool2d(x, k=2):
     # MaxPool2D wrapper
     return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],
                           padding='SAME')
 
+#this is the function that defines the conv-net computation graph
 def conv_net(x, weights, biases, dropout):
     # MNIST data input is a 1-D vector of 784 features (28*28 pixels)
     # Reshape to match picture format [Height x Width x Channel]
     # Tensor input become 4-D: [Batch Size, Height, Width, Channel]
     x = tf.reshape(x, shape=[-1, 28, 28, 3])
     
-    conv01=conv2d(x, weights['wc01'], biases['bc01'])
+    conv01=conv2d(x, weights['wc01'], biases['bc01']) #no maxpool
        
-    conv02=conv2d(conv01, weights['wc02'], biases['bc02'])
+    conv02=conv2d(conv01, weights['wc02'], biases['bc02']) #no maxpool
     # Convolution Layer
     conv1 = conv2d(conv02, weights['wc1'], biases['bc1'])
     # Max Pooling (down-sampling)
@@ -74,7 +81,7 @@ weights = {
     'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
     # fully connected, 7*7*64 inputs, 1024 outputs
     'wd1': tf.Variable(tf.random_normal([7*7*64, 1024])),
-    # 1024 inputs, 10 outputs (class prediction)
+    # 1024 inputs, 4 outputs (class prediction)
     'out': tf.Variable(tf.random_normal([1024, num_classes]))
 }
 
